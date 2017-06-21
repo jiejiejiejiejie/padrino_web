3f481bf54cae17700a52526b46f2df8bde393118
luojie2017 <luojie@yeezon.com>
2017-06-21 13:26:13 +0800

    feat

---
+++ app/helpers/main_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module PadrinoWeb
+  class App
+    module MainHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers MainHelper
+  end
+end
